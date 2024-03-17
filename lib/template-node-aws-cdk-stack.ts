5e64fd8215d2cc25ca32bb6aaa7268c03ca331f5
Igor Gonak <igor.gonak@bonprix.net>
2024-03-17 12:38:49 +0100

    prepare for slack integration

--- lib/template-node-aws-cdk-stack.ts
+++ lib/template-node-aws-cdk-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import path = require('path');
 
 export class TemplateNodeAwsCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TemplateNodeAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const listUsersLambda = new Function(this, 'CreateUser', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'index.handler',
+            code: Code.fromAsset(path.join('./lambda/user')),
+        });
+    }
 }
